7d1966f28d5127b7e553a43ce3bb8a4485b537a3
unknown <peculiarworth@gmail.com>
2023-03-19 15:03:48 +0100

    added background color, improved on margin and line spacing of my simple form

---
+++ main.js
@@ -0,0 +1,49 @@
+document.querySelector("#demo").innerHTML="change the content"
+//for alert
+//alert("This is a simple alert message")
+//for console
+/*console.log("This is a message to log to the console")
+const userName="Pipe Aremo"
+userName="Jane"
+console.log(userName)
+*/
+/*let age="23"
+age=46
+console.log(age)
+*/
+//Two methods of creating a function
+/*function sayHello(){
+    alert("Hello World")
+}
+sayHello()*/
+/*const sayHello = ()=>{
+    alert("Hello User")
+}
+sayHello()
+*/
+/*const sayHello = (userNmae)=>{
+    alert=("Hello+userNmae")
+}
+sayHello("Pipe Aremo")
+*/
+//addition of two numbers
+/*const sayHello = (a,b)=>{
+    return a + b
+}
+var answer = sayHello(20,50)
+alert(answer)
+*/
+let age = 18
+let fullName = "Aremo Pipe"
+/*if (age >= 18 && fullName == "Aremo Pipe"){
+    alert("you are allowed")
+}else{
+    alert("you are not allowed")
+}
+*/
+if (age >= 18 || fullName!= "Aremo Pipe"){
+    alert ("You are allowed")
+}else{
+    alert("You are not allowed")
+}
+
